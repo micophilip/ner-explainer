ea9e3c55ab70fc51a6dc7b11e4b1ea136ba085c7
mico <micophilip@gmail.com>
2021-03-11 20:20:36 -0500

    remove squad loading

--- script.py
+++ script.py
@@ -105,151 +105,135 @@ criterion = nn.MSELoss()
 
 pp = pprint.PrettyPrinter(indent=4)
 
-# input_data is a list of dictionary which has a paragraph and questions
-with open("/content/drive/My Drive/train-v2.0.json") as f:
-    squad = json.load(f)
-    for article in squad["data"]:
-        # input_data = []
-        # i = 1
-        for context_questions in article["paragraphs"]:
+pad_token_label_id = CrossEntropyLoss().ignore_index
+examples = read_examples_from_file(args.data_dir, mode)
 
-            input_data = []
-            i = 1
+features = convert_examples_to_features(
+    examples,
+    labels,
+    args.max_seq_len,
+    tokenizer,
+    cls_token_at_end=bool(args.model_type in ["xlnet"]),
+    cls_token=tokenizer.cls_token,
+    cls_token_segment_id=2 if args.model_type in ["xlnet"] else 0,
+    sep_token=tokenizer.sep_token,
+    sep_token_extra=bool(args.model_type in ["roberta"]),
+    pad_on_left=bool(args.model_type in ["xlnet"]),
+    pad_token=tokenizer.pad_token_id,
+    pad_token_segment_id=tokenizer.pad_token_type_id,
+    pad_token_label_id=pad_token_label_id,
+)
 
-            paragraphs = {"id": i, "text": context_questions["context"]}
-            paragraphs["ques"] = [(x["question"], x["is_impossible"]) for x in context_questions["qas"]]
-            input_data.append(paragraphs)
-            i += 1
+all_input_ids = torch.tensor([f.input_ids for f in features], dtype=torch.long)
+all_input_mask = torch.tensor([f.input_mask for f in features], dtype=torch.long)
+all_segment_ids = torch.tensor([f.segment_ids for f in features], dtype=torch.long)
+all_label_ids = torch.tensor([f.label_ids for f in features], dtype=torch.long)
 
-            pad_token_label_id = CrossEntropyLoss().ignore_index
-            examples = read_examples_from_file(input_data)
+eval_dataset = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_label_ids)
 
-            features = convert_examples_to_features(
-                examples,
-                labels,
-                args.max_seq_len,
-                tokenizer,
-                cls_token_at_end=bool(args.model_type in ["xlnet"]),
-                cls_token=tokenizer.cls_token,
-                cls_token_segment_id=2 if args.model_type in ["xlnet"] else 0,
-                sep_token=tokenizer.sep_token,
-                sep_token_extra=bool(args.model_type in ["roberta"]),
-                pad_on_left=bool(args.model_type in ["xlnet"]),
-                pad_token=tokenizer.pad_token_id,
-                pad_token_segment_id=tokenizer.pad_token_type_id,
-                pad_token_label_id=pad_token_label_id,
-            )
+prefix = ""
+args.eval_batch_size = args.per_gpu_eval_batch_size * max(1, args.n_gpu)
+# Note that DistributedSampler samples randomly
+eval_sampler = SequentialSampler(eval_dataset) if args.local_rank == -1 else DistributedSampler(
+    eval_dataset)
+eval_dataloader = DataLoader(eval_dataset, sampler=eval_sampler, batch_size=args.eval_batch_size)
 
-            all_input_ids = torch.tensor([f.input_ids for f in features], dtype=torch.long)
-            all_input_mask = torch.tensor([f.input_mask for f in features], dtype=torch.long)
-            all_segment_ids = torch.tensor([f.segment_ids for f in features], dtype=torch.long)
-            all_label_ids = torch.tensor([f.label_ids for f in features], dtype=torch.long)
+# multi-gpu evaluate
+if args.n_gpu > 1:
+    model = torch.nn.DataParallel(model)
 
-            eval_dataset = TensorDataset(all_input_ids, all_input_mask, all_segment_ids, all_label_ids)
-            ############################################################################################################
-            prefix = ""
-            args.eval_batch_size = args.per_gpu_eval_batch_size * max(1, args.n_gpu)
-            # Note that DistributedSampler samples randomly
-            eval_sampler = SequentialSampler(eval_dataset) if args.local_rank == -1 else DistributedSampler(
-                eval_dataset)
-            eval_dataloader = DataLoader(eval_dataset, sampler=eval_sampler, batch_size=args.eval_batch_size)
+# Eval!
+logger.info("***** Running evaluation %s *****", prefix)
+logger.info("  Num examples = %d", len(eval_dataset))
+logger.info("  Batch size = %d", args.eval_batch_size)
+eval_loss = 0.0
+nb_eval_steps = 0
+preds = None
+out_label_ids = None
+model.eval()
+for batch in tqdm(eval_dataloader, desc="Evaluating"):
+    batch = tuple(t.to(args.device) for t in batch)
 
-            # multi-gpu evaluate
-            if args.n_gpu > 1:
-                model = torch.nn.DataParallel(model)
+    input_ids = batch[0]
+    attention_mask = batch[1]
+    segment_ids = batch[2]
+    batch_labels = batch[3]
 
-            # Eval!
-            logger.info("***** Running evaluation %s *****", prefix)
-            logger.info("  Num examples = %d", len(eval_dataset))
-            logger.info("  Batch size = %d", args.eval_batch_size)
-            eval_loss = 0.0
-            nb_eval_steps = 0
-            preds = None
-            out_label_ids = None
-            model.eval()
-            for batch in tqdm(eval_dataloader, desc="Evaluating"):
-                batch = tuple(t.to(args.device) for t in batch)
+    with torch.no_grad():
+        inputs = {"input_ids": batch[0], "attention_mask": batch[1], "labels": batch[3]}
+        if args.model_type != "distilbert":
+            inputs["token_type_ids"] = (
+                batch[2] if args.model_type in ["bert", "xlnet"] else None
+            )  # XLM and RoBERTa don"t use segment_ids
+        outputs = model(**inputs)
+        tmp_eval_loss, logits = outputs[:2]
 
-                input_ids = batch[0]
-                attention_mask = batch[1]
-                segment_ids = batch[2]
-                batch_labels = batch[3]
+        if args.n_gpu > 1:
+            tmp_eval_loss = tmp_eval_loss.mean()  # mean() to average on multi-gpu parallel evaluating
 
-                with torch.no_grad():
-                    inputs = {"input_ids": batch[0], "attention_mask": batch[1], "labels": batch[3]}
-                    if args.model_type != "distilbert":
-                        inputs["token_type_ids"] = (
-                            batch[2] if args.model_type in ["bert", "xlnet"] else None
-                        )  # XLM and RoBERTa don"t use segment_ids
-                    outputs = model(**inputs)
-                    tmp_eval_loss, logits = outputs[:2]
+        eval_loss += tmp_eval_loss.item()
+    nb_eval_steps += 1
+    if preds is None:
+        preds = logits.detach().cpu().numpy()
+        out_label_ids = inputs["labels"].detach().cpu().numpy()
+    else:
+        preds = np.append(preds, logits.detach().cpu().numpy(), axis=0)
+        out_label_ids = np.append(out_label_ids, inputs["labels"].detach().cpu().numpy(), axis=0)
+    explainer = BertExplainer(model)
+    # TODO What actually goes into the explainer?
+    relevance, attentions, self_attentions = explainer.explain(input_ids, segment_ids, attention_mask,
+                                                               [o["span"] for o in out_label_ids.values()])
 
-                    if args.n_gpu > 1:
-                        tmp_eval_loss = tmp_eval_loss.mean()  # mean() to average on multi-gpu parallel evaluating
+    input_tensor = torch.stack(
+        [r.sum(-1).unsqueeze(-1) * explainer.layer_values_global["bert.encoder"]["input"][0] for r in
+         relevance], 0)
+    target_tensor = torch.stack(relevance, 0).sum(-1)
+    encoder_loss = train_autoencoder(input_tensor, target_tensor, encoder,
+                                     decoder, encoder_optimizer, decoder_optimizer, criterion,
+                                     max_length=13)
+    print('Encoder loss: %.4f' % encoder_loss)
 
-                    eval_loss += tmp_eval_loss.item()
-                nb_eval_steps += 1
-                if preds is None:
-                    preds = logits.detach().cpu().numpy()
-                    out_label_ids = inputs["labels"].detach().cpu().numpy()
-                else:
-                    preds = np.append(preds, logits.detach().cpu().numpy(), axis=0)
-                    out_label_ids = np.append(out_label_ids, inputs["labels"].detach().cpu().numpy(), axis=0)
-                explainer = BertExplainer(model)
-                # TODO What actually goes into the explainer?
-                relevance, attentions, self_attentions = explainer.explain(input_ids, segment_ids, attention_mask,
-                                                                           [o["span"] for o in out_label_ids.values()])
+eval_loss = eval_loss / nb_eval_steps
+preds = np.argmax(preds, axis=2)
 
-                input_tensor = torch.stack(
-                    [r.sum(-1).unsqueeze(-1) * explainer.layer_values_global["bert.encoder"]["input"][0] for r in
-                     relevance], 0)
-                target_tensor = torch.stack(relevance, 0).sum(-1)
-                encoder_loss = train_autoencoder(input_tensor, target_tensor, encoder,
-                                                 decoder, encoder_optimizer, decoder_optimizer, criterion,
-                                                 max_length=13)
-                print('Encoder loss: %.4f' % encoder_loss)
+label_map = {i: label for i, label in enumerate(labels)}
 
-            eval_loss = eval_loss / nb_eval_steps
-            preds = np.argmax(preds, axis=2)
+out_label_list = [[] for _ in range(out_label_ids.shape[0])]
+predictions = [[] for _ in range(out_label_ids.shape[0])]
 
-            label_map = {i: label for i, label in enumerate(labels)}
+for i in range(out_label_ids.shape[0]):
+    for j in range(out_label_ids.shape[1]):
+        if out_label_ids[i, j] != pad_token_label_id:
+            out_label_list[i].append(label_map[out_label_ids[i][j]])
+            predictions[i].append(label_map[preds[i][j]])
 
-            out_label_list = [[] for _ in range(out_label_ids.shape[0])]
-            predictions = [[] for _ in range(out_label_ids.shape[0])]
+results = {
+    "loss": eval_loss,
+    "precision": precision_score(out_label_list, predictions),
+    "recall": recall_score(out_label_list, predictions),
+    "f1": f1_score(out_label_list, predictions),
+}
 
-            for i in range(out_label_ids.shape[0]):
-                for j in range(out_label_ids.shape[1]):
-                    if out_label_ids[i, j] != pad_token_label_id:
-                        out_label_list[i].append(label_map[out_label_ids[i][j]])
-                        predictions[i].append(label_map[preds[i][j]])
+logger.info("***** Eval results %s *****", prefix)
+for key in sorted(results.keys()):
+    logger.info("  %s = %s", key, str(results[key]))
 
-            results = {
-                "loss": eval_loss,
-                "precision": precision_score(out_label_list, predictions),
-                "recall": recall_score(out_label_list, predictions),
-                "f1": f1_score(out_label_list, predictions),
-            }
+if mode == "test":
+    print(classification_report(out_label_list, predictions))
 
-            logger.info("***** Eval results %s *****", prefix)
-            for key in sorted(results.keys()):
-                logger.info("  %s = %s", key, str(results[key]))
+# For printing the results ####
+# TODO Needs to be accustomed for NER
+index = None
+for example in examples:
+    if index != example.example_id:
+        pp.pprint(example.para_text)
+        index = example.example_id
+        print('\n')
+        print(colored('***********Question and Answers *************', 'red'))
 
-            if mode == "test":
-                print(classification_report(out_label_list, predictions))
-
-            # For printing the results ####
-            # TODO Needs to be accustomed for NER
-            index = None
-            for example in examples:
-                if index != example.example_id:
-                    pp.pprint(example.para_text)
-                    index = example.example_id
-                    print('\n')
-                    print(colored('***********Question and Answers *************', 'red'))
-
-                ques_text = colored(example.question_text + " Unanswerable: " + str(example.unanswerable), 'blue')
-                print(ques_text)
-                prediction = colored(predictions[math.floor(example.unique_id / 9)][example]['text'], 'green',
-                                     attrs=['reverse', 'blink'])
-                print(prediction)
-                print('\n')
+    ques_text = colored(example.question_text + " Unanswerable: " + str(example.unanswerable), 'blue')
+    print(ques_text)
+    prediction = colored(predictions[math.floor(example.unique_id / 9)][example]['text'], 'green',
+                         attrs=['reverse', 'blink'])
+    print(prediction)
+    print('\n')
